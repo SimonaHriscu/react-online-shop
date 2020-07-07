5fe22e47bd2e9179969a101f1e4237489ec4188b
Simona <see.moona@yahoo.com>
2020-07-07 18:53:36 +0200

    cart added

--- src/components/total/Total.jsx
+++ src/components/total/Total.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React from "react";
 
-const Total =(props)=>{
-   // console.log(props.itemInfo)
-   
-return(
-    <div className="total">This is total{props.itemInfo}</div>
-)
-
-}
-
+const Total = (props) => {
+  if (props.showCart) {
+    const items = props.products.map((item, i) => (
+      <li key={item.id}>
+        <span> {item.icon}</span>
+        <span> {item.productName}</span> 
+        <span> {item.price}$</span>
+      </li>
+    ));
+    console.log(items);
+    return (
+      <div className="total">
+        <h3>Total number of items: {props.cartPriceHandle}</h3>
+        <ul className="cart-products">
+          <p>Your items:</p>
+          <li>
+            <span>
+              {" "}
+              {"No"} :{""}
+            </span>{" "}
+            <span> {"product"}</span> <span>{"price"}</span>
+          </li>
+          {items}
+        </ul>
+        <h3>Total:  {props.price} $</h3>
+      </div>
+    );
+  }
+  return props.showCart;
+};
 
 export default Total;
