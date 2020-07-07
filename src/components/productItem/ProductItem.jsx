1dc905aa0960cfa1a3130c1f2ec0f17e9c9d3396
Simona <see.moona@yahoo.com>
2020-07-07 18:55:39 +0200

    structure

---
+++ src/components/productItem/ProductItem.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const  ProductItem =(props)=> {
+  
+//console.log(props);
+  
+    const { id, productName, icon, price, inventory } = props.info;
+ 
+  
+     
+    return (
+      <React.Fragment>
+        <li key={id}>
+          <p>{productName} <i>{icon}</i></p>
+          <h5>{price}<span> $</span></h5>
+          <button onClick={e=> props.cartItem( {id, productName, icon, price, inventory})} disabled={inventory === 0}>
+            {inventory > 0 ? "Add to cart" : "Sold out"}
+          </button>
+        </li>
+      </React.Fragment>
+    );
+  }
+
+export default ProductItem
